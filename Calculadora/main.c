7b45d388c3161a32ab99711b72b2c767dbc92491
Daniel Rodriguez Chaves <daniel.rodriguez.chaves@intel.com>
2023-11-13 17:23:35 -0600

    Funcion de Suma y commit inicial
    Ya creé el proyecto en codeblocks y agregué una función simple de suma.

---
+++ Calculadora/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "suma.h"
+
+int main()
+{
+    printf("Hello world!\n");
+    suma(1,2);
+    suma(3,4);
+    suma(5,6);
+    return 0;
+}
---
+++ Calculadora/suma.h
@@ -0,0 +1,8 @@
+#ifndef SUMA_H_INCLUDED
+#define SUMA_H_INCLUDED
+
+void suma(int a, int b){
+printf("%d mas %d = %d\n", a, b, a+b);
+}
+
+#endif // SUMA_H_INCLUDED
